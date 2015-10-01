841a6221b3c30b2022c22e2f00821566fab3f473
Ye (Eric) Wang <ericwang921198@gmail.com>
2015-10-01 14:36:49 -0400

    lab 6

---
+++ lab6_script_full.R
@@ -0,0 +1,63 @@
+# Lab 6
+# Created by Ye (Eric) Wang
+# on 10/01/2015
+# In this lab we will analyze a real-world dataset on housing values on suburbs of Boston
+
+# library
+#******************************************************
+# <put any libraries that you need here>
+library(MASS)
+#******************************************************
+
+# import data
+#******************************************************
+# 1. directly import the dataset from http://archive.ics.uci.edu/ml/datasets/Housing
+# 2. briefly read the data description at http://archive.ics.uci.edu/ml/machine-learning-databases/housing/housing.names
+#    and manipulate your data frame to only contain CRIM, PTRATIO and MEDV. (Don't forget to properly name your columns)
+housing <- read.table("http://archive.ics.uci.edu/ml/machine-learning-databases/housing/housing.data")
+housing <- housing[,c(1,11,14)]
+names(housing) <- c("CRIM","PTRATIO","MEDV")
+#******************************************************
+
+# EDA
+#******************************************************
+# 1. draw a pairwaise scatterplot of the three variables considered,
+#    do you see a correlation between CRIM and MEDV and PTRATIO and MEDV?
+#    what do you think of the normality of the data?
+# 2. do a quick linear regression of MEDV agains CRIM and PTRATIO, check the model 
+#    assumptions
+dev.off()
+pairs(housing)
+lm0 <- lm(MEDV~.,data=housing)
+par(mfrow=c(2, 2))
+plot(lm0)
+#******************************************************
+
+# transform the response variable
+#******************************************************
+# 1. choose the best box-cox transformation and do the regression again 
+#    with the transformed response, check the model assumptions again
+# 2. explain the coefficients
+dev.off()
+boxcox(lm0)
+lm1 <- lm(log(MEDV)~.,data=housing)
+par(mfrow=c(2, 2))
+plot(lm1)
+summary(lm1)
+#******************************************************
+
+# identifying the most influential point
+#******************************************************
+# 1. find the index of that point
+# 2. draw two scatterplots, MEDV against CRIM and MEDV against PTRATIO, and annotate the influential point in theplots
+# 3. delete that point from the dataset and redo regression analysis
+# 4. is there a big difference between the coefficients with and without the influential point?
+# 5. what should you do when such influential point exist?
+dev.off()
+plot(housing$CRIM,housing$MEDV)
+text(housing$CRIM[381], housing$MEDV[381], labels="381", cex= 0.7, pos=3)
+plot(housing$PTRATIO,housing$MEDV)
+text(housing$PTRATIO[381], housing$MEDV[381], labels="381", cex= 0.7, pos=3)
+lm2 <- lm(log(MEDV)~.,data=housing[-381,])
+summary(lm2)
+#******************************************************
